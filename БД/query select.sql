2305b0c9e89472baf8fbcad2a015f7290e0cbef6
M00nl1ght07 <denis72728282@yandex.ru>
2024-12-11 02:38:12 +0300

    добавлены запросы select к бд

---
+++ БД/query select.sql
@@ -0,0 +1,7 @@
+TRUNCATE partners_import, material_type_import, product_type_import, products_import, partner_products_import;
+
+select * from partners_import
+select * from material_type_import
+select * from product_type_import
+select * from products_import
+select * from partner_products_import
